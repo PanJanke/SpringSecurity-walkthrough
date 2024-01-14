7c0798742e2dd1b0dc1c3fe026b8e42b38aaeabb
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-14 22:10:28 +0100

    storing user credentials using jdbc

--- src/main/java/pl/jano/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/pl/jano/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,12 +2,18 @@ package pl.jano.learnspringsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,9 +24,7 @@ public class BasicAuthSecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
-                auth -> {
-                    auth.anyRequest().authenticated();
-                });
+                auth -> auth.anyRequest().authenticated());
 
         http.sessionManagement(
                 session ->
@@ -32,25 +36,39 @@ public class BasicAuthSecurityConfiguration {
 
         http.csrf(csrf -> csrf.disable());
 
+        http.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailService() {
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService(DataSource dataSource) {
 
         var user = User.withUsername("jano")
                 .password("{noop}dummy")
-                .roles(String.valueOf(Roles.USER))
+                .roles("USER")
                 .build();
-
 
         var admin = User.withUsername("admin")
                 .password("{noop}dummy")
-                .roles(String.valueOf(Roles.ADMIN))
+                .roles("ADMIN", "USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
+
 
 
 }
